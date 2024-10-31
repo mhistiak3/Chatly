d1c8037ebdf7d0e02b898b206f230a8265d851e6
Istiak <mhistiak2@gmail.com>
2024-10-31 19:01:39 +0600

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ import helmet from "helmet";
 import { APP_PORT } from "./config/config.js";
 import userRouters from "./routes/user.routes.js";
 import defultErrorHandler from "./middleware/default.error.handler.js";
+import { connectDB } from "./config/mogodb.config.js";
 
 // app object
 const app = express();
@@ -27,6 +28,7 @@ app.use("/api/v1/user", userRouters);
 // defult error handler
 app.use(defultErrorHandler);
 // listen port
-app.listen(APP_PORT, () => {
+app.listen(APP_PORT, async () => {
+  await connectDB("mongodb://localhost:27017/");
   console.log("Server is running on port 3000");
 });
