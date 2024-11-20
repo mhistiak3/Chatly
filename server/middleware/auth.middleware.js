77fa91abb934d7cd09e4b4265155a1a4933e6d8e
Istiak <mhistiak2@gmail.com>
2024-11-20 16:40:59 +0600

    update auth middleware

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 import { JWT_SECRET } from "../config/config.js";
 import customErrorHandler, {
   TryCatch,
@@ -12,6 +13,11 @@ export const isAuthenticated = TryCatch(async (req, res, next) => {
   const decoded = jwt.verify(token, JWT_SECRET);
   if (!decoded) {
     return customErrorHandler(res, "You are not logged in", 401);
+  }
+  // find user and check id is exist
+  const user = await User.findById(decoded.userId);
+  if (!user) {
+    return customErrorHandler(res, "User not found, please login", 404);
   }
   req.userId = decoded.userId;
   next();
